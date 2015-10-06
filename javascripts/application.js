3bca55b5f448cb7816986d8e070317db2609bf40
arijoe <ari.weitzman@gmail.com>
2015-10-05 22:17:33 -0700

    Initialize repository with set files, basic JSON parsing.

---
+++ javascripts/application.js
@@ -0,0 +1,47 @@
+// data type of this anatomy:
+// {
+//     "id": 103594,
+//     "field_id": 100002,
+//     "start_date": "2015-05-11",
+//     "end_date": "2015-05-21",
+//     "start_time": "18:15:00",
+//     "end_time": "20:15:00",
+//     "sunday": 0,
+//     "monday": 1,
+//     "tuesday": 1,
+//     "wednesday": 0,
+//     "thursday": 0,
+//     "friday": 1,
+//     "saturday": 0,
+//     "stage_id": 107469,
+//     "field_name": "Edgely",
+//     "field_surface": "grass"
+// },
+
+var days = [
+  'sunday',
+  'monday',
+  'tuesday',
+  'wednesday',
+  'thursday',
+  'friday',
+  'saturday'
+]
+
+days.forEach( function( day ) {
+  $("<ul class='weekday'" + "id=" + day + ">" + day + "</ul>").appendTo( $( ".weekdays" ) );
+});
+
+
+data.timeslots.forEach( function( timeslot ) {
+  days.forEach( function( day ) {
+    if ( timeslot[day] === 1 ) {
+      $( "#" + day ).append( "<li>" + timeslot.field_name + ': ' + timeslot.start_time + "</li>");
+    }
+  });
+});
+
+// $( "<ul/>", {
+//   "class": "scheduling",
+//   html: timeslots.join( "" )
+// }).appendTo( "body" );
