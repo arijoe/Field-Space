5a4a9c611ba854341cc5852c2b307e2c5502f695
arijoe <ari.weitzman@gmail.com>
2015-11-01 16:30:07 -0800

    Begin week component, separate JS work into functions.

--- javascripts/content.js
+++ javascripts/content.js
@@ -18,8 +18,32 @@
 //     "field_surface": "grass"
 // },
 
-// Change start_time value into human-readable format
-var formatTime = function ( timeObj ) {
+var days = [
+  'sunday',
+  'monday',
+  'tuesday',
+  'wednesday',
+  'thursday',
+  'friday',
+  'saturday'
+],
+months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
+
+// Change time value into human-readable format
+var formatTime = function (timeObj) {
   var hours = timeObj.slice(0, 2);
   var minutes = timeObj.slice(3, 5);
   var meridian = ' AM';
@@ -34,81 +58,114 @@ var formatTime = function ( timeObj ) {
   return hours + ':' + minutes + meridian;
 };
 
-// Add days of the week to DOM
-var days = [
-  'sunday',
-  'monday',
-  'tuesday',
-  'wednesday',
-  'thursday',
-  'friday',
-  'saturday'
-]
+// Change date value into more accessible-readable format
+var formatDate = function (dateObj) {
+  var date = dateObj.split("-");
+  return [months[date[1] - 1], date[2], date[0], dateObj, Date.parse(new Date(dateObj))];
+};
 
-days.forEach( function( day ) {
-  $("<ul/>", {
-    "class": day + " weekday",
-    html: "<h4 class=heading>" + day + "</h2>"
-  }).appendTo( $( ".weekdays" ) )
-  .css( "text-transform", "capitalize" );
-});
+var dateRange = function () {
+  var starts = {}, ends = {}, minStart, maxEnd;
 
-// Add time values into days of the week
-days.forEach( function( day ) {
-  var startTimes = {};
+  data.timeslots.forEach( function (timeslot, idx) {
+    starts[ Date.parse(timeslot.start_date) ] = idx;
+    ends[ Date.parse(timeslot.end_date) ] = idx;
+  });
 
-  data.timeslots.forEach( function( timeslot ) {
-    var timeStart = formatTime( timeslot.start_time ),
-    timeEnd = formatTime( timeslot.end_time ),
-    flag = true;
+  minTime = data.timeslots[starts[ Math.min.apply( null, Object.keys(starts) ) ]];
+  maxTime = data.timeslots[ends[ Math.max.apply( null, Object.keys(ends) ) ]];
 
-    // If a timeslot is valid for that day...
-    if ( timeslot[day] === 1 ) {
+  return [minTime.start_date, maxTime.end_date];
+};
 
-      // ...and other fields already exist for that time, add the field;...
-      $.each( startTimes, function (startTime, fields) {
-        if ( startTime === timeStart && flag ) {
-          fields.push(
+///////////////////////
+
+var addWeeks = function () {
+  var range = dateRange(), min = new Date(range[0]),
+    max = new Date(range[1]), minDay = min.getDay(), maxDay = max.getDay(),
+    count = 0;
+
+  $("<div/>", {
+    "class":  count.toString() + " week",
+    html: "<div class='weekdays'><h2>" + min + "</h2></div>"
+  }).appendTo( $(".weeks") );
+};
+
+var addDays = function () {
+  days.forEach( function( day ) {
+    
+    // Add days of the week to DOM
+    $("<ul/>", {
+      "class": day + " weekday",
+      html: "<h3 class=heading>" + day.slice(0,3) + "</h3>"
+    }).appendTo( $(".weekdays") )
+    .css( "text-transform", "capitalize" );
+
+    // Add time values into days of the week
+    var startTimes = {};
+
+    data.timeslots.forEach( function( timeslot ) {
+      var timeStart = formatTime(timeslot.start_time),
+      timeEnd = formatTime(timeslot.end_time),
+      dateStart = formatDate(timeslot.start_date),
+      dateEnd = formatDate(timeslot.end_date),
+      flag = true;
+
+      // If a timeslot is valid for that day...
+      if ( timeslot[day] === 1 ) {
+
+        // ...and other fields already exist for that time, add the field;...
+        $.each( startTimes, function (startTime, fields) {
+          if ( startTime === timeStart && flag ) {
+            fields.push(
+              [
+                timeslot.field_name,
+                timeEnd,
+                timeslot.start_date,
+                timeslot.end_date
+              ]
+             );
+            flag = false;
+          }
+        });
+
+        // ...else, add a new start-time for that day containing that field...
+        if ( flag ) {
+          startTimes[timeStart] = [
             [
               timeslot.field_name,
               timeEnd,
               timeslot.start_date,
               timeslot.end_date
             ]
-           );
-          flag = false;
+          ];
         }
+      } // end of if statement matching timeslot to day of week
+    }); // end of timeslots iterator
+
+    // ...now add each start-time-to-fields matching to the DOM
+    $.each(startTimes, function (startTime, fieldData) {
+      var timeEnd = fieldData[0][1],
+      $el = $("<li/>", {
+        "class": "timeslot",
+        html: "<h6 class='time'>" +
+              startTime + " - " + timeEnd + "</h6>"
       });
 
-      // ...else, add a new start-time for that day containing that field...
-      if ( flag ) {
-        startTimes[timeStart] = [
-          [
-            timeslot.field_name,
-            timeEnd,
-            timeslot.start_date,
-            timeslot.end_date
-          ]
-        ];
-      }
-    } // end of if statement matching timeslot to day of week
-  }); // end of timeslots iterator
+      $( "." + day ).append($el);
 
-  // ...now add each start-time-to-fields matching to the DOM
-  $.each(startTimes, function (startTime, fieldData) {
-    var timeEnd = fieldData[0][1],
-    $el = $("<li/>", {
-      "class": "timeslot",
-      html: "<h6 class='time'>" +
-            startTime + " - " + timeEnd + "</h6>"
-    });
+      fieldData.forEach( function (arr) {
+        $el.append(
+          "<span class='field'>" + arr[0] + "</span>"
+        );
+      });
+    }); // end of DOM addition iterator
+  });   // end of days iterator
+};      // end of function
 
-    $( "." + day ).append($el);
+/////////////////////
 
-    fieldData.forEach( function (arr) {
-      $el.append(
-        "<span class='field'>" + arr[0] + "</span>"
-      );
-    });
-  }); // end of DOM addition iterator
-});   // end of days iterator
+$(document).on("ready", function () {
+  addWeeks();
+  addDays();
+});
